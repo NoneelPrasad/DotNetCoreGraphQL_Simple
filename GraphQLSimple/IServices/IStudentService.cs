785a1f50df4acb4ae5a6801c505e9b5395044aa8
Noneel Prasad <prasad_no@usp.ac.fj>
2021-02-08 11:57:51 +1200

    Initial Commit

---
+++ GraphQLSimple/IServices/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using GraphQLSimple.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLSimple.IServices
+{
+    public interface IStudentService
+    {
+        List<Student> GetStudents();
+    }
+}
---
+++ GraphQLSimple/Model/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLSimple.Model
+{
+    public class Student
+    {
+        public Guid StudentId { get; set; }
+
+        public string FirstName { get; set; }
+        public string OtherNames { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
