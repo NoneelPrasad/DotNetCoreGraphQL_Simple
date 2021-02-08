06cb8887d1ff8ae5330c21cec9057de734dcbb30
Noneel Prasad <prasad_no@usp.ac.fj>
2021-02-08 12:45:34 +1200

    graphQL playground with student data

---
+++ GraphQLSimple/Service/StudentService.cs
@@ -0,0 +1,32 @@
+﻿using GraphQLSimple.IServices;
+using GraphQLSimple.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLSimple.Service
+{
+    public class StudentService : IStudentService
+    {
+        public List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            for(int i = 0; i < 10; i++)
+            {
+                students.Add(new Student() 
+                {
+                    StudentId = new Guid(),
+                    FirstName = $"Student {i}",
+                    LastName = $"Student {i}",
+                    OtherNames = $"Student {i}",
+
+                });
+
+            }
+
+            return students;
+            //throw new NotImplementedException();
+        }
+    }
+}
